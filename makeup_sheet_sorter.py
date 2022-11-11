8ecbbef5d390fe5dfcc090302c790b027496dd2e
JNaeder <39416919+JNaeder@users.noreply.github.com>
2022-11-11 14:08:57 -0500

    Put colors class in it's own file

--- makeup_sheet_sorter.py
+++ makeup_sheet_sorter.py
@@ -5,18 +5,7 @@ import io
 import os
 from dateutil import parser
 import time
-
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+from bcolors import *
 
 
 class Makeup_Sheet_Sorter:
